3729ec992b6c0a792e8e10c81a25ac4bf068d22f
Gahyun Son <gahyunson@Gahyuns-MacBook-Pro.local>
2023-03-30 02:48:07 +0900

    test

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: "sk-",
 });
 const openai = new OpenAIApi(configuration);
 
