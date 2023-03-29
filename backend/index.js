9fb41ca291d08352b2b54bd1a4b8f3264bbb72dc
Gahyun Son <gahyunson@Gahyuns-MacBook-Pro.local>
2023-03-30 04:10:58 +0900

    backend openai api call, frontend chatting ui + send button function

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,48 @@
-const { Configuration, OpenAIApi } = require("openai");
+// chat gpt api 
+// import
+const { Configuration, OpenAIApi } = require("openai"); 
 
 const configuration = new Configuration({
-  apiKey: "sk-",
+  apiKey: "sk-", //gitignore 사용 
 });
 const openai = new OpenAIApi(configuration);
 
 async function apiCall(){
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Hello world",
-      });
-    console.log(completion.data.choices[0].text);
+    const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    max_tokens : 250, 
+    temperature : 0.5, 
+    messages: [
+
+],
+});
+// console.log(completion.data.choices[0].message);
+console.log(completion.data.choices[0].message['content']);
+}
+apiCall()
+
+// web server 구축 
+const express = require('express')
+var cors = require('cors')
+const app = express()
+
+
+// cors 아무데서나 요청오는 것을 방지. 어디서 요청온지 확인해준다.
+//cors issue 해결 
+let corsOptions = {
+    origin: 'https://www.domain.com',
+    credentials: true
 }
 
-apiCall()
+app.use(cors(corsOptions));
+
+// post 요청을 받을 수 있는 부분 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
+
+// POST method route
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage');
+  });
+
+app.listen(3000)
