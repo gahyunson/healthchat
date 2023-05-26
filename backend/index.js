02ca82a92d524896d91d26ae30e6d870a34096b0
Gahyun <73574914+gahyunson@users.noreply.github.com>
2023-05-26 18:05:26 +0900

    connecting mysql at node.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const config = require('./config.js');
 const apiKey = config.apiKey;
-const promptmessage = config.promptmessage;
-const serverless = require('serverless-http'); //Express 앱을 AWS Lambda 함수로 변환
+const sqlPassword = config.sqlPassword;
+// const promptmessage = config.promptmessage;
+// const serverless = require('serverless-http'); //Express 앱을 AWS Lambda 함수로 변환
 
 const { Configuration, OpenAIApi } = require("openai");
 const express = require('express')
@@ -10,14 +11,14 @@ const app = express()
 
 const configuration = new Configuration({
     apiKey: apiKey,
-    promptmessage: promptmessage
+    // promptmessage: promptmessage
   });
 const openai = new OpenAIApi(configuration);
 
 //CORS 이슈 해결
 let corsOptions = {
-    // origin: 'https://healthcat.pages.dev',
-    origin: "*",
+    origin: 'https://healthcat.pages.dev/',
+    // origin: "*",
     credentials: true,
 };
 app.use(cors(corsOptions));
@@ -26,10 +27,26 @@ app.use(cors(corsOptions));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+var mysql = require('mysql2');
+var sql = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: sqlPassword,
+  database: 'healthchat'
+});
+sql.connect(function(err){
+  if (err) console.log('err.message',err.message);
+  console.log('Connected!');
+  var sqlInsert = "INSERT INTO Questions (id, question_text, created_at) VALUES (0,'sample','2023-05-26 09:29:58.827')";
+  sql.query(sqlInsert, function(err,result){
+    if (err) throw err;
+    console.log('inserted!');
+  })
+});
 // POST method route
 app.post('/', async function (req, res) {
     let {userMessages, assistantMessages} = req.body
-    let messages = promptmessage
+    let messages = [{role: "system", content: "당신은 재활 전문가입니다. 당신은 사람들이 몸에 통증을 느끼면 마사지 혹은 운동, 스트레칭 방법을 추천해줍니다. 의사나 물리치료사 수준의 의학적 지식을 가지고 있습니다. 우리 몸의 각 관절 부위별로 수술 후 재활단계에 대해서 설명할 수 있습니다. 아픈 부위에 대해서 마사지 부위 혹은 운동 방법을 알려줍니다. 어떤 움직임을 할 때 통증이 있는 부위에 대해 설명해줍니다. 아픈부위 주위의 근육 운동 혹은 스트레칭 방법을 알려줍니다."}]
 
     while (userMessages.length != 0 || assistantMessages.length != 0) {
         if (userMessages.length != 0) {
@@ -47,7 +64,6 @@ app.post('/', async function (req, res) {
     let retries = 0;
     let completion;
     while (retries < maxRetries) {
-    //   console.log("while");
       try {
         completion = await openai.createChatCompletion({
           model: "gpt-3.5-turbo",
@@ -60,11 +76,8 @@ app.post('/', async function (req, res) {
           console.log(`Error fetching data, retrying (${retries}/${maxRetries})...`);
       }
     }
-    // console.log(completion);
     let result = completion.data.choices[0].message.content
-    // assistantMessages.push(result);
-    console.log("result:",result);
-    // console.log("assistantMessages:",assistantMessages);
+    assistantMessages.push(result);
     res.json({"assistant": result});
 });
 
