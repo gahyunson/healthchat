0418d7bcd73abededac70ad6cfa15d8ce2d7f7ab
Gahyun <73574914+gahyunson@users.noreply.github.com>
2023-05-26 20:14:43 +0900

    request -> question_text, response -> answer_text로 connecting mysql at nodejs

--- backend/index.js
+++ backend/index.js
@@ -34,19 +34,13 @@ var sql = mysql.createConnection({
   password: sqlPassword,
   database: 'healthchat'
 });
-sql.connect(function(err){
-  if (err) console.log('err.message',err.message);
-  console.log('Connected!');
-  var sqlInsert = "INSERT INTO Questions (id, question_text, created_at) VALUES (0,'sample','2023-05-26 09:29:58.827')";
-  sql.query(sqlInsert, function(err,result){
-    if (err) throw err;
-    console.log('inserted!');
-  })
-});
+
 // POST method route
 app.post('/', async function (req, res) {
     let {userMessages, assistantMessages} = req.body
+    question_text = userMessages.slice(-1)[0]
     let messages = [{role: "system", content: "당신은 재활 전문가입니다. 당신은 사람들이 몸에 통증을 느끼면 마사지 혹은 운동, 스트레칭 방법을 추천해줍니다. 의사나 물리치료사 수준의 의학적 지식을 가지고 있습니다. 우리 몸의 각 관절 부위별로 수술 후 재활단계에 대해서 설명할 수 있습니다. 아픈 부위에 대해서 마사지 부위 혹은 운동 방법을 알려줍니다. 어떤 움직임을 할 때 통증이 있는 부위에 대해 설명해줍니다. 아픈부위 주위의 근육 운동 혹은 스트레칭 방법을 알려줍니다."}]
+    /* userMessages : 질문글 , messages : 모든 채팅 내용(system 포함), */
 
     while (userMessages.length != 0 || assistantMessages.length != 0) {
         if (userMessages.length != 0) {
@@ -77,8 +71,20 @@ app.post('/', async function (req, res) {
       }
     }
     let result = completion.data.choices[0].message.content
+    // result -> answer_text
     assistantMessages.push(result);
     res.json({"assistant": result});
+
+    sql.connect(function(err){
+      if (err) console.log('err:',err.message);
+      console.log('Connected at SQL!');
+      var sqlInsert = `INSERT INTO Questions (id, question_text, answer_text) 
+                        VALUES (0, '${question_text}','${result}')`;
+      sql.query(sqlInsert, function(err, result){
+        if (err) throw err;
+        console.log('inserted!');
+      })
+    })
 });
 
 // module.exports.handler = serverless(app);
